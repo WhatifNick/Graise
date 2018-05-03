97cd2c43a79c6948383df0543f6ee34fa57c4073
Nick Pasquariello <npasquariello@gmail.com>
2018-05-03 14:34:14 +1000

    Added Devise and locations to users

--- app/models/area.rb
+++ app/models/area.rb
@@ -1,4 +1,5 @@
 class Area < ApplicationRecord
   has_many :users
   has_many :venues
+  validates :city, :state, :country, :post_code, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,16 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  validates :first_name, :last_name, presence: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   belongs_to :area
   has_and_belongs_to_many :causes
   has_many :events
+
   #
-  # after_create :set_area
+   # after_create :set_area
   #
   # def set_area
   #   self.area = Area.create
